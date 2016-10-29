0d429fe7f53784411b341c79a56ecd371c25eacc
Mauricio Labarca <mauriciolabarca4tw@gmail.com>
2016-10-29 14:49:50 -0300

    Add link to signup form with placeholder

---
+++ src/public/sign-up/sign-up.controller.js
@@ -0,0 +1,15 @@
+(function (){
+"use strict"
+
+angular.module('public')
+.controller('SignUpController', SignUpController);
+
+// Todo
+//SignUpController.$inject = [''];
+function SignUpController(){
+  var signupCtrl = this;
+  
+}
+
+
+})()
